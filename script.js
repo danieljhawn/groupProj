16370ed5b6ecdd33fd2316247277d42b47c8d2df
Daniel Hawn <danieljhawn@gmail.com>
2019-11-19 18:13:55 -0600

    adds CSS

--- script.js
+++ script.js
@@ -2,29 +2,29 @@ var queryURL = "https://api.thecatapi.com/v1/images/search";
 
 $("#cat-button").on("click", function(event) {
     $.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function (catInfo) {
-    var imageURL = catInfo[0].url;
-    var imageHeight = catInfo[0].height;
-    var imageWidth = catInfo[0].width;
-    console.log("image url is " + imageURL);
-    $("#cat-image").attr("src", imageURL);
-    console.log("image height is " + imageHeight);
-    console.log("image width is " + imageWidth);
-    getQRCodeImage(imageURL);
+        url: queryURL,
+        method: "GET"
+    }).then(function(catInfo) {
+        var imageURL = catInfo[0].url;
+        var imageHeight = catInfo[0].height;
+        var imageWidth = catInfo[0].width;
+        console.log("image url is " + imageURL);
+        $("#cat-image").attr("src", imageURL);
+        console.log("image height is " + imageHeight);
+        console.log("image width is " + imageWidth);
+        getQRCodeImage(imageURL);
 
-});
+    });
 });
 
 
 function getQRCodeImage(URL) {
     var QRCodeQueryURL = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + URL;
     $.ajax({
-    url: QRCodeQueryURL,
-    method: "GET"
-}).then(function (QRURL) {
-    console.log("QR code URL is " + QRCodeQueryURL);
-    $("#qr-image").attr("src", QRCodeQueryURL);
-});
+        url: QRCodeQueryURL,
+        method: "GET"
+    }).then(function(QRURL) {
+        console.log("QR code URL is " + QRCodeQueryURL);
+        $("#qr-image").attr("src", QRCodeQueryURL);
+    });
 }
